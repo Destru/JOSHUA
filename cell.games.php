b7fe493c867b88c923e0f4d1a5bb942d8e9c8cd0
Alexander Stoever <alexander@howaboutwe.com>
2014-06-30 13:03:40 -0400

    TSW icons shouldn't be a one-off thing forever.

--- cell.games.php
+++ cell.games.php
@@ -115,12 +115,12 @@ function api($game, $api) {
 			$actives = ''; $passives = '';
 			$iconSize = '24';
 			foreach($api->actives as $slot) {
-				$actives .= '<div class="tsw-actives image icon" title="'.$slot->name.'" style="display:inline-block;width:'.$iconSize.'px;height:'.$iconSize.'px;margin-right:5px;padding:2px;">'.
+				$actives .= '<div class="image icon light" title="'.$slot->name.'" style="display:inline-block;width:'.$iconSize.'px;height:'.$iconSize.'px;margin-right:5px;padding:2px;">'.
 					'<img src="'.$slot->image->icon.'" class="icon" width="'.$iconSize.'" height="'.$iconSize.'">'.
 					'</div>';
 			}
 			foreach($api->passives as $slot) {
-				$passives .= '<div class="tsw-passives image icon" title="'.$slot->name.'" style="display:inline-block;width:'.$iconSize.'px;height:'.$iconSize.'px;margin-right:5px;padding:2px;">'.
+				$passives .= '<div class="image icon dark" title="'.$slot->name.'" style="display:inline-block;width:'.$iconSize.'px;height:'.$iconSize.'px;margin-right:5px;padding:2px;">'.
 					'<img src="'.$slot->image->icon.'" class="icon" width="'.$iconSize.'" height="'.$iconSize.'">'.
 					'</div>';
 			}
